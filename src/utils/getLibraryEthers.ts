63f9503bac6af5489e0222c23a5152b5e9d0ba46
Karan U Kanthawar <karankanthawar@gmail.com>
2022-04-21 17:54:04 +0530

    refator: Remove unnecessary console logs

--- src/utils/getLibraryEthers.ts
+++ src/utils/getLibraryEthers.ts
@@ -1,9 +1,4 @@
 function getLibraryEthers(provider: any) {
-  console.log({ provider });
-  // const libraryEther = new Web3Provider(provider);
-  // libraryEther.pollingInterval = 15000;
-  // console.log({ libraryEther });
-  // return libraryEther;
   return provider;
 }
 
--- src/utils/getLibraryWeb3.ts
+++ src/utils/getLibraryWeb3.ts
@@ -1,7 +1,6 @@
 import Web3 from "web3";
 
 const getLibraryWeb3 = (provider: any): Web3 => {
-  console.log({ provider });
   return provider;
 };
 
